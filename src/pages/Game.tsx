53316e4c4051ba838a8591e00a6ea72832084a89
alongre <alongre@gmail.com>
2020-09-30 17:44:02 +0300

    adding score

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyledChar, StyledGame, StyledScore, StyledTimer } from '../styled/Game';
 import { Strong } from '../styled/Utils';
 
 export default function Game() {
+	const [score, setScore] = useState(1);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setScore((prevScore) => prevScore + 1);
+		}, [1000]);
+		return () => {
+			clearInterval(interval);
+		};
+	}, [score]);
+
 	return (
 		<StyledGame>
 			<StyledScore>
-				Score:<Strong>0</Strong>
+				Score:<Strong>{score}</Strong>
 			</StyledScore>
 			<StyledChar>A</StyledChar>
 			<StyledTimer>
